8870d2f601f96f77c32326cc47c38a1a05227fc1
Eli Sacks <bastlifa@gmail.com>
2019-06-25 01:13:24 -0400

    css refactoring, making intermediate responsiveness, struggle struggle

--- js/index.js
+++ js/index.js
@@ -11,15 +11,15 @@ TweenMax.fromTo(".arrow-up", 0.8, {y: 80, opacity: 0}, {y: 0, opacity: 1, delay:
 
 //hover anim for feature cards
 
-let featureCards = document.querySelectorAll(".feature");
+// let featureCards = document.querySelectorAll(".feature");
 
-featureCards.forEach(card => card.addEventListener("mouseover", _ => 
-{
-    TweenMax.to(card, 0.5, {y: -10, scale: 1.03, boxShadow: '0 0 20px rgba(0,0,0,0.4)'});
-}));
+// featureCards.forEach(card => card.addEventListener("mouseover", _ => 
+// {
+//     TweenMax.to(card, 0.5, {y: -10, scale: 1.03, boxShadow: '0 0 20px rgba(0,0,0,0.4)'});
+// }));
 
-featureCards.forEach(card => card.addEventListener("mouseleave", _ => 
-{
-    TweenMax.to(card, 0.5, {y: 7, scale: 1.03, boxShadow: '0 0 20px rgba(0,0,0,0)'});
-}));
+// featureCards.forEach(card => card.addEventListener("mouseleave", _ => 
+// {
+//     TweenMax.to(card, 0.5, {y: 7, scale: 1.03, boxShadow: '0 0 20px rgba(0,0,0,0)'});
+// }));
 
