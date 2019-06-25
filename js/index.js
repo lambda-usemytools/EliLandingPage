1bbd9bfdd822cf3e61eeb35bedff8fbdc7bd184e
Eli Sacks <bastlifa@gmail.com>
2019-06-25 12:41:48 -0400

    remade buttons as divs, direct to create account, email mailto, hover effect on btns

--- js/index.js
+++ js/index.js
@@ -23,3 +23,21 @@ featureCards.forEach(card => card.addEventListener("mouseleave", _ =>
     TweenMax.to(card, 0.5, {y: 7, scale: 1.03, boxShadow: '0 0 20px rgba(0,0,0,0)'});
 }));
 
+let joinBtns = Array.from(document.querySelectorAll(".join-today-button"));
+
+if(document.querySelector(".sign-up-now-button"))
+{
+    joinBtns.push(document.querySelector(".sign-up-now-button"));
+}
+
+joinBtns.forEach(btn => btn.addEventListener("mouseenter", _ =>
+{
+    btn.style.border = "3px solid cyan";
+    btn.style.boxShadow = "0 0 20px rgba(0,0,0,0.3)";
+}))
+
+joinBtns.forEach(btn => btn.addEventListener("mouseleave", _ =>
+{
+    btn.style.border = "3px solid transparent";
+    btn.style.boxShadow = "0 0 0 rgba(0,0,0,0)";
+}))
