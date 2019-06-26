c5a0b045032a802c2eeb878cb4e26367fc131fbe
Eli Sacks <bastlifa@gmail.com>
2019-06-26 11:48:07 -0400

    mobile menu slides out, about page started

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -7,17 +7,19 @@ let menuIsOpen = false;
 menuIcon.addEventListener("click", _ =>
 {
     mobileMenu.classList.add("mobile-menu-show");
-    TweenMax.fromTo(mobileMenu, .7, {x:200}, {x:0})
+    TweenMax.fromTo(mobileMenu, .5, {x:200}, {x:0})
     menuIsOpen = true;
 })
 
 document.querySelector("body").addEventListener("click", _ =>
 {
+    
     // if (menuIsOpen && (event.target !== mobileMenu && event.target !== menuIcon && !burgerLines.includes(event.target)))
-    if(menuIsOpen && !menuItems.includes(event.target))
+    if(menuIsOpen && !menuItems.includes(event.target) && event.target != menuIcon)
     {
         event.preventDefault();
-        mobileMenu.classList.remove("mobile-menu-show");
+        let closeMenu = () => mobileMenu.classList.remove("mobile-menu-show");
+        TweenMax.to(mobileMenu, .5, {x:200, onComplete: closeMenu});
         menuIsOpen = false;
     }
 })
